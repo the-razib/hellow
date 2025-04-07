fa96342b2f7084db7ddde41c7d8187724134cc0e
Md Razib <mdrazib.cse@gmail.com>
2025-04-07 10:18:26 +0600

    2

---
+++ lib/new.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Hellow extends StatefulWidget {
+  const Hellow({super.key});
+
+  @override
+  State<Hellow> createState() => _HellowState();
+}
+
+class _HellowState extends State<Hellow> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
